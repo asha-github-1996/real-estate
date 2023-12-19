bd08f097782f768f5684dfdf31f2346481de28b2
asha <ashapoulose0@gmail.com>
2023-12-19 12:23:18 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,14 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-const signup = async (req, res, next) => {
+//SignUp
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   //Hashing password
   const hashedPassword = bcryptjs.hashSync(password, 10);
-
   const newUser = new User({ username, email, password: hashedPassword });
-
   try {
     //Saving to database
     await newUser.save();
@@ -16,7 +17,28 @@ const signup = async (req, res, next) => {
     });
   } catch (error) {
     next(error);
-    //next(errorHandler(550, "error from the function"));
   }
 };
-export default signup;
+
+//SIgnIn
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(404, "Wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    //To avoid sending password to user
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
