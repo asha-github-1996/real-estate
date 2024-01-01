8d9f1db11c4c04d8e99aa1afda406e843f91fb96
asha <ashapoulose0@gmail.com>
2024-01-01 21:19:42 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Real Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime totam
+        libero, ipsum in voluptatem itaque? Nesciunt dignissimos, neque quas
+        vitae quam, labore qui iste voluptatibus distinctio saepe velit, nisi
+        quos?
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime totam
+        libero, ipsum in voluptatem itaque? Nesciunt dignissimos, neque quas
+        vitae quam, labore qui iste voluptatibus distinctio saepe velit, nisi
+        quos?
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime totam
+        libero, ipsum in voluptatem itaque? Nesciunt dignissimos, neque quas
+        vitae quam, labore qui iste voluptatibus distinctio saepe velit, nisi
+        quos?
+      </p>
+    </div>
+  );
 };
 
 export default About;
