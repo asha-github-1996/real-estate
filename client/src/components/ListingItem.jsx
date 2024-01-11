3013173235b632b3546bfb3254f62e7ef7fb4fa7
asha <ashapoulose0@gmail.com>
2024-01-11 19:03:19 +0530

    Modified ListingItem page & verifyUser.js

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -4,7 +4,7 @@ import { MdLocationOn } from "react-icons/md";
 
 const ListingItem = ({ listing }) => {
   return (
-    <div className="bg-white shadow-md hover:shadow-lg transistion-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
+    <div className="bg-white shadow-md hover:shadow-lg transistion-shadow overflow-hidden rounded-lg w-full sm:w-[270px]">
       <Link to={`/listing/${listing._id}`}>
         <img
           src={
