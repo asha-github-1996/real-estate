986745de4ce11c29ee9edb1e92d3c14670f559b3
asha <ashapoulose0@gmail.com>
2024-01-15 22:21:26 +0530

    corrected listing price

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -30,9 +30,10 @@ const ListingItem = ({ listing }) => {
           </p>
           <p className="text-slate-500 mt-2 font-semibold">
             $
-            {listing.offer
+            {/* {listing.offer
               ? listing.discountPrice.toLocaleString("en-US")
-              : listing.regularPrice.toLocaleString("en-US")}
+              : listing.regularPrice.toLocaleString("en-US")} */}
+            {listing.regularPrice.toLocaleString("en-US")}
             {listing.type === "rent" && " / month"}
           </p>
           <div className="txt-slate-700 flex gap-4">
