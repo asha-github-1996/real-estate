98de0827777c437679516c5fe2794faef57b9ea1
asha-github-1996 <102643998+asha-github-1996@users.noreply.github.com>
2024-01-15 15:56:44 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">House </span>&nbsp;
+            <span className="text-slate-500">Home </span>&nbsp;
             <span className="text-slate-700">ForYou</span>
           </h1>
         </Link>
