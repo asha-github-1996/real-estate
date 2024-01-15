466fbcd725d09043ded34de779808ab9eee2b76a
asha-github-1996 <102643998+asha-github-1996@users.noreply.github.com>
2024-01-15 15:57:03 +0530

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,11 +6,11 @@ const About = () => {
       {/* <h1 className="text-3xl font-bold mb-4 text-slate-800">House</h1> */}
 
       <h1 className="font-bold text-3xl flex flex-wrap mb-4">
-        <span className="text-slate-500">House </span>&nbsp;
+        <span className="text-slate-500">Home </span>&nbsp;
         <span className="text-slate-700">ForYou</span>
       </h1>
       <p className="mb-4 text-slate-700">
-        House For You is a leading real estate agency that specializes in
+        Home For You is a leading real estate agency that specializes in
         helping clients buy, sell, and rent properties in the most desirable
         neighborhoods. Our team of experienced agents is dedicated to providing
         exceptional service and making the buying and selling process as smooth
